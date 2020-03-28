03dcfc51e58593f6823f3bcf16eb9ff61b1bf4ba
Sifir <47905155+UltimusDev@users.noreply.github.com>
2020-03-28 12:06:34 -0500

    Create test.sh

---
+++ Mac/test.sh
@@ -0,0 +1,6 @@
+#Template Shell Script for running a PowerShell Script
+#Follow our tutorial at https://www.sifirtech.com/how-to-run-a-powershell-script-by-double-clicking/
+
+#! /usr/local/bin/pwsh
+
+pwsh ~/desktop/test.ps1
