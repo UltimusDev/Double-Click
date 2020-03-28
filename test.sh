f89c0127f3b7a82ba6a56192a5f35cffbdc10c3b
Sifir <47905155+UltimusDev@users.noreply.github.com>
2020-03-28 12:08:21 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#Template Shell Script for running a PowerShell Script
-#Follow our tutorial at https://www.sifirtech.com/how-to-run-a-powershell-script-by-double-clicking/
-
-#! /usr/local/bin/pwsh
-
-pwsh ~/desktop/test.ps1
